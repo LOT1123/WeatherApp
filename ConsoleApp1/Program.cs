cfd073728d87efd6339f9e136ff6404c094fbad0
macaroni <tiago2lot@gmail.com>
2025-07-07 18:56:01 -0700

    added some celsius to fahrenheit functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double myTemp = 24.5;
+        double myTempFahrenheit = ConvertCToF(myTemp);
+        double checkOurWork = ConvertFToC(myTempFahrenheit);
+
+        Console.WriteLine(myTemp);
+        Console.WriteLine(myTempFahrenheit);
+        Console.WriteLine(checkOurWork);
     }
+
+    static double ConvertCToF(double celsius)
+    {
+        return celsius * (9.0 / 5.0) + 32;
+    }
+
+    static double ConvertFToC(double fahrenheit)
+    {
+        return (fahrenheit - 32) * (5.0 / 9.0);
+    }
+
 }
